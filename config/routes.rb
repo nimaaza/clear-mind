3263bab97ce96e5949572da59770360095899aff
Nima Azarbayjani <nimaza@gmail.com>
2020-11-15 18:01:15 +0330

    added the basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resouces :doctors, only: [:index, :show] do
+    resouces :appointments, only: :create
+    resouces :reviews, only: :create
+  end
+
+  get '/my_appointments', to: 'appointments#my_appointments'
 end
+
