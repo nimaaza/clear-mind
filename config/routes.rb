08c0425c4eca356ddfd89a244d774d8902cfbe16
Nima Azarbayjani <nimaza@gmail.com>
2020-11-15 18:21:23 +0330

    corrected typos in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resouces :doctors, only: [:index, :show] do
-    resouces :appointments, only: :create
-    resouces :reviews, only: :create
+  resources :doctors, only: [:index, :show] do
+    resources :appointments, only: :create
+    resources :reviews, only: :create
   end
 
   get '/my_appointments', to: 'appointments#my_appointments'
 end
-
