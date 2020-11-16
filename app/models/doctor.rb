52fe8a1658dd8c50114e29227acb7ac2099cd3c9
Nima Azarbayjani <nimaza@gmail.com>
2020-11-16 11:39:17 +0330

    added validations and associations to the user, doctor, review, and appointment models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,7 @@
 class Doctor < ApplicationRecord
+  validates :specialization, :first_name, :last_name,
+            :email, :phone_number, presence: true
+
+  has_many :reviews
+  has_many :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :email, :phone_number, presence: true
+
+  has_many :appointments
+  has_many :reviews
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
