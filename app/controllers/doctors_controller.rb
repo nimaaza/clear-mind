8ffd00e0ce117b1fce20119a10f30266bad79108
Nima Azarbayjani <nimaza@gmail.com>
2020-11-16 12:47:58 +0330

    initial implementation of show for doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,3 +1,18 @@
 class DoctorsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+
+  def show
+    @doctor = Doctor.find(params[:id])
+    @reviews = @doctor.reviews
+    @articles = @doctor.articles
+    @specializations = JSON.parse(@doctor.specializations)
+
+    @rating = 0
+
+    @reviews.each do |review|
+      @rating += review.rating
+    end
+
+    @rating = (@rating / @reviews.size).round(2)
+  end
 end
