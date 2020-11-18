7d97cd941d3427c0b60570dc04cb851cf95009e1
Nima Azarbayjani <nimaza@gmail.com>
2020-11-18 16:37:07 +0330

    code refactoring in doctors' controller and model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -7,4 +7,44 @@ class Doctor < ApplicationRecord
   has_many :articles
 
   has_one_attached :avatar
+
+  def free_appointments
+    forthcoming_appointments = appointments
+                               .where('appointment_start > ?', Time.now)
+                               .order(appointment_start: :asc)
+    make_white_list(forthcoming_appointments).to_json
+  end
+
+  def average_rating
+    rating = 0.0
+    reviews.each { |review| rating += review.rating }
+    reviews.empty? ? 0 : (rating / reviews.size).round(1)
+  end
+
+  private
+
+  def make_white_list(appointments)
+    white_list = {}
+
+    appointments.each do |appointment|
+      start_time = appointment.appointment_start.getlocal
+      key = start_time.strftime("%Y-%m-%d")
+      start_hour = start_time.hour
+
+      white_list[key] = make_work_hours(appointment) unless white_list.key?(key)
+      white_list[key].delete(start_hour)
+    end
+
+    white_list
+  end
+
+  def make_work_hours(appointment)
+    start_time = appointment.appointment_start.getlocal
+
+    if start_time.strftime("%Y-%m-%d") == Time.now.strftime("%Y-%m-%d")
+      ((Time.now.hour + 1)..18).to_a
+    else
+      (9..18).to_a
+    end
+  end
 end
