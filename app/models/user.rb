18fa67d165110076edb4e5b31c3528c0ded847eb
Nima Azarbayjani <nimaza@gmail.com>
2020-11-22 19:48:26 +0330

    added method to user model to get ongoing appointment

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.current_appointment(user)
+    now = Time.now
+    start = Time.new(now.year, now.month, now.day, now.hour, 0, 0)
+    next_hour = Time.new(now.year, now.month, now.day, now.hour + 1, 0, 0)
+    user.appointments.where('appointment_start = ? and appointment_end = ?', start, next_hour)
+  end
 end
