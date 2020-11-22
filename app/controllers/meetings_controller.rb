fe564bf1efb7359d6c10f9484b162a926b0b7aaa
Nima Azarbayjani <nimaza@gmail.com>
2020-11-22 19:52:33 +0330

    basic setup for creating meetings using OpenTok (TokBox) API

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,30 @@
+require 'opentok'
+
 class MeetingsController < ApplicationController
   def meet
+    # appointment = User.current_appointment(current_user)
+    appointment = Appointment.first
 
+    if appointment.nil?
+      #
+    else
+      load_meeting_variables appointment
+    end
+  end
+
+  private
+
+  def load_meeting_variables(appointment)
+    opentok = OpenTok::OpenTok.new Figaro.env.opentok_api_key, Figaro.env.opentok_api_secret
+
+    if appointment.session_id.present?
+      @session_id = appointment.session_id
+    else
+      @session_id = opentok.create_session.session_id
+      appointment.update(session_id: @session_id)
+    end
+
+    @api_key = Figaro.env.opentok_api_key
+    @token = opentok.generate_token @session_id
   end
 end
