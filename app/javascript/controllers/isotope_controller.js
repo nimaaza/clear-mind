499c5eba54f430f072b7c068d92090e4b73684b6
Nima Azarbayjani <nimaza@gmail.com>
2020-11-26 10:39:31 +0330

    add all button to isotope button group + change the color

--- app/javascript/controllers/isotope_controller.js
+++ app/javascript/controllers/isotope_controller.js
@@ -1,8 +1,5 @@
-const specializations = ['ADHD', 'Addiction', 'Anxiety', 'Bipolar',
-                         'Borderline', 'Depression', 'Family', 'Grief',
-                         'Relationship', 'Self-esteem', 'Sex-therapy',
-                         'Stress', 'Trauma', 'PTSD'];
-
+const specializations = ['ADHD', 'Addiction', 'Anxiety', 'Bipolar', 'Borderline', 'Depression', 'Family', 'Grief',
+                         'Relationship', 'Self-esteem', 'Sex-therapy', 'Stress', 'Trauma', 'PTSD'];
 
 const initIsotope = () => {
   const elem = document.querySelector('.isotope-grid');
@@ -14,7 +11,7 @@ const initIsotope = () => {
   const divSpecializations = document.querySelector('#div-specializations');
 
   specializations.forEach((specialization) => {
-    const button = `<button type="button" class="isotope-filter-buttons btn btn-secondary">${specialization}</button>`;
+    const button = `<button type="button" class="isotope-filter-buttons btn">${specialization}</button>`;
     divSpecializations.insertAdjacentHTML('beforeend', button);
   });
 
@@ -23,12 +20,17 @@ const initIsotope = () => {
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
       iso.arrange({
-        filter: function( itemElem ) {
-          return itemElem.classList.contains(button.textContent);
-        }
+        filter: gridItem => gridItem.classList.contains(button.textContent)
       });
     });
   });
+
+  const allButton = `<button type="button" id="isotope-filter-buttons-all" class="isotope-filter-buttons btn">All Specializations</button>`;
+  divSpecializations.insertAdjacentHTML('afterbegin', allButton);
+  document.querySelector('#isotope-filter-buttons-all')
+    .addEventListener('click', (event) => {
+      iso.arrange({ filter: gridItem => true });
+    });
 };
 
 export { initIsotope };
